d8673bbb9649b34bd498204edd25a29f2f6287f9
Travis Haynes <travis@hi5dev.com>
2015-09-16 11:30:27 -0700

    simplified Config#load_yaml; wrote a test for Config

--- lib/local_postal/config.rb
+++ lib/local_postal/config.rb
@@ -1,11 +1,16 @@
 module LocalPostal::Config
-  def self.load_yaml(*path)
-    path = path.map(&:to_s)
-    path[path.length-1] += '.yml'
-    yml = File.read(File.join(root, *path))
+  # Reads a .yml file from the config folder.
+  #
+  # @param [Symbol] name The name of the .yml file without its extension.
+  def self.load_yaml(name)
+    yml = File.read(File.join(root, "#{name}.yml"))
+
     YAML.load(yml)
   end
 
+  # The full path name for the config folder.
+  #
+  # @return [String] A path name.
   def self.root
     @root ||= File.expand_path(File.join('../../../config'), __FILE__)
   end
